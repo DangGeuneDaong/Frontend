15ff6cb8776367455ba1939bac39d22e6446042f
geniee1220 <geniee1220@gmail.com>
2023-06-06 11:45:55 +0900

    Design : 색상, 폰트 시스템을 전역 변수로 추가

--- src/assets/styles/theme.ts
+++ src/assets/styles/theme.ts
@@ -1,3 +1,32 @@
 export const theme = {
-  backgroundColor: 'blue',
+  color: {
+    primary: '#F8BF52',
+  },
+  font: {
+    // sm, md, lg, xl
+    heading_xl: `
+      font-weight: 600;
+      font-size: 30px;
+    `,
+    heading_lg: `
+      font-weight: 600;
+      font-size: 25px;
+    `,
+    heading_md: `
+      font-weight: 600;
+      font-size: 20px;
+    `,
+    contents: `
+      font-weight: 400;
+      font-size: 14px;
+    `,
+    hashtag: `
+      font-weight: 300;
+      font-size: 14px;
+    `,
+    button: `
+      font-weight: 600;
+      font-size: 14px;
+    `,
+  },
 };
