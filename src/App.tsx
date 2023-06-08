58a6b79bb9e338196514a8b49ead9d4ce8488742
eeseohyun <jinlijinyoung@gmail.com>
2023-06-08 18:43:53 +0900

    Feat : 회원가입 Route 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import MainPage from './pages/MainPage/MainPage';
+import JoinPage from './pages/JoinPage';
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<MainPage />} />
+
+      <Route path="/register" element={<JoinPage />} />
     </Routes>
   );
 }
