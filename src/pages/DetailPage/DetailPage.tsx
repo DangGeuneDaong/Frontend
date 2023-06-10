c2b7fc6096be8598216997463e7bc8c39cc85427
geniee1220 <geniee1220@gmail.com>
2023-06-10 21:18:34 +0900

    Fix : 하위 컴포넌트에 전달할 수 있도록 setProps 추가

--- src/pages/DetailPage/DetailPage.tsx
+++ src/pages/DetailPage/DetailPage.tsx
@@ -5,39 +5,45 @@ import S from './DetailPage.styles';
 import ImageCarouselArea from '../../components/ImageCarouselArea';
 import PostArea from '../../components/PostArea';
 import CommentArea from '../../components/CommentArea';
-import MainTemplate from '../../components/template/MainTemplate'
+import MainTemplate from '../../components/template/MainTemplate';
+import { useState } from 'react';
 
 const config = [
   {
     title: 'image one',
-    image: 'https://images.unsplash.com/photo-1472552944129-b035e9ea3744?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80'
+    image:
+      'https://images.unsplash.com/photo-1472552944129-b035e9ea3744?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80',
   },
   {
     title: 'image two',
-    image: 'https://images.unsplash.com/photo-1597843786186-826cc3489f56?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=764&q=80'
+    image:
+      'https://images.unsplash.com/photo-1597843786186-826cc3489f56?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=764&q=80',
   },
   {
     title: 'image three',
-    image: 'https://images.unsplash.com/photo-1616668983570-a971956d8928?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=686&q=80'
-  }
-]
+    image:
+      'https://images.unsplash.com/photo-1616668983570-a971956d8928?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=686&q=80',
+  },
+];
 
 const DetailPage = () => {
+  const [comment, setComment] = useState<string>('');
+
   return (
     <MainTemplate>
       <S.Container>
         <ImageCarouselArea config={config} />
         <PostArea
-          nickname={"whale2200d"}
-          location={"대구 달서구 대곡동"}
-          productName={"웰시코기 유기농 사료"}
-          firstCategory={"강아지"}
-          secondCategory={"사료"}
-          uploadTime={"1달 전"}
-          productDetails={"안녕하세요! 웰시코기 유기농 사료 나눔합니다!"}
+          nickname={'whale2200d'}
+          location={'대구 달서구 대곡동'}
+          productName={'웰시코기 유기농 사료'}
+          firstCategory={'강아지'}
+          secondCategory={'사료'}
+          uploadTime={'1달 전'}
+          productDetails={'안녕하세요! 웰시코기 유기농 사료 나눔합니다!'}
         />
-        <CommentArea />
-      </S.Container >
+        <CommentArea setProps={(comment) => setComment(comment)} />
+      </S.Container>
     </MainTemplate>
   );
 };
