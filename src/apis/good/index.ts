928c7683fc4580b6bbe4f59ae2849ec46529764f
geniee1220 <geniee1220@gmail.com>
2023-06-13 21:36:29 +0900

    Feat : 이미지 업로드 API를 유틸 함수로 생성

--- src/apis/good/index.ts
+++ src/apis/good/index.ts
@@ -1,7 +1,38 @@
 import { AxiosInstance } from 'axios';
 import axiosInstance from '..';
 
-// 폼 전송
+// 이미지 업로드
+export const uploadImage = async (selecteeFiles: File[]) => {
+  const instance: AxiosInstance = axiosInstance();
+  const formData = new FormData();
+
+  // 업로드 한 이미지가 없다면 빈 배열을 return
+  if (selecteeFiles.length === 0) return [];
+
+  try {
+    console.log('파일 업로드 시작');
+
+    //selectedItem의 각 아이템을 formData로 가공
+    for (let i = 0; i < selecteeFiles.length; i++) {
+      formData.append(`image${i}`, selecteeFiles[i]);
+    }
+
+    // S3에서 각각의 이미지에 대해 String 값으로 return
+    // const response = await instance.post('/api/img/upload', formData);
+    const response = await instance.post(
+      'http://localhost:3001/image',
+      formData
+    );
+
+    // return response;
+    return ['https://image.com/1', 'https://image.com/2'];
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+};
+
+// 글 등록
 export const addPost = async (data: any) => {
   try {
     const instance: AxiosInstance = axiosInstance();
