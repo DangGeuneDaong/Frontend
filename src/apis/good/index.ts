ddcce11ad07f6415cf242b56eff67779c0f62f03
geniee1220 <geniee1220@gmail.com>
2023-06-14 22:18:05 +0900

    Refactor : 응답 Interceptor 설정하고 에러 핸들링 구문 이동

--- src/apis/good/index.ts
+++ src/apis/good/index.ts
@@ -45,19 +45,5 @@ export const addPost = async (data: any) => {
     return response;
   } catch (error: any) {
     console.log('에러 발생', error);
-
-    switch (error.response.status) {
-      // 400 (Bad Request) 401 (Unauthorized) 404 (Not Found) 500 (Internal Server Error)
-      case 400:
-      case 404:
-        throw new Error('API 요청에 실패했습니다.');
-      case 401:
-        // TODO : 비 로그인 유저일 때, 확인 알럿 띄우고 로그인 페이지로 이동
-        throw new Error('로그인이 필요합니다.');
-      case 500:
-        throw new Error('서버에 오류가 발생했습니다.');
-      default:
-        throw new Error('알 수 없는 에러가 발생했습니다.');
-    }
   }
 };
--- src/apis/index.ts
+++ src/apis/index.ts
@@ -11,6 +11,32 @@ export const axiosInstance = () => {
     },
   });
 
+  // 인터셉터 설정
+
+  // API 응답 데이터 가공 및 에러 핸들링
+  instance.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    (error) => {
+      if (error.response && error.response.status) {
+        switch (error.response.status) {
+          case 400:
+          case 404:
+            throw new Error('API 요청에 실패했습니다.');
+          case 401:
+            // TODO : 비 로그인 유저일 때, 확인 알럿 띄우고 로그인 페이지로 이동
+            throw new Error('로그인이 필요합니다.');
+          case 500:
+            throw new Error('서버에 오류가 발생했습니다.');
+          default:
+            throw new Error('알 수 없는 에러가 발생했습니다.');
+        }
+      } else {
+        throw new Error('An unknown error occurred.');
+      }
+    }
+  );
   return instance;
 };
 
