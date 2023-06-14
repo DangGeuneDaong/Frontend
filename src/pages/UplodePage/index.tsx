1e1a4816c21e2e7a2c8850e14e68bdfc2361fda4
geniee1220 <geniee1220@gmail.com>
2023-06-14 20:33:37 +0900

    Refactor : 해시태그 등록 및 삭제 함수 제외

--- src/pages/UplodePage/index.tsx
+++ src/pages/UplodePage/index.tsx
@@ -12,7 +12,6 @@ import * as S from './styles';
 import Input from '../../components/Form/Input';
 import Button from '../../components/Button';
 import Textarea from '../../components/Form/Textarea';
-import Hashtag from '../../components/Hashtag';
 import Dropdown from '../../components/Dropdown';
 import MultiUploader from '../../components/FileUploader/MultiUploader';
 import { addPost, uploadImage } from '../../apis/good';
@@ -68,18 +67,6 @@ function UplodePage() {
     }
   };
 
-  // 태그 추가
-  const handleKeyUp = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    const inputTag = watch('hashtag');
-    const newTags = addTag(tags, inputTag, e, setValue, setFocus, setError);
-    setTags(newTags);
-  };
-
-  // 태그 삭제 버튼 클릭 시 태그 삭제
-  const handleTagDelete = (tag: string) => {
-    setTags(deleteTag(tags, tag));
-  };
-
   // 폼 전송
   const { mutateAsync: uploadImagesMutation } = useMutation(uploadImage);
   const { mutate: addPostMutation } = useMutation(addPost);
@@ -96,14 +83,12 @@ function UplodePage() {
         sub_category: selectedProduct,
         title: data.title,
         description: data.description,
-        // latitude : 위도
-        // longitude :  경도
+        // location : 주소
         status: '판매중',
         good_image_list: uploadedImages,
         view_cnt: 0,
         created_at: new Date(),
         modified_at: new Date(),
-        // hashtag: tags,
       };
 
       addPostMutation(postData);
@@ -175,29 +160,6 @@ function UplodePage() {
                   }
                 }}
               ></Textarea>
-            </S.InputContainer>
-
-            {/* 해시태그 입력 */}
-            <S.InputContainer>
-              <S.Title>해시태그</S.Title>
-
-              <S.HashtagContainer>
-                {tags.map((tag, index) => (
-                  <Hashtag
-                    tag={tag}
-                    key={index}
-                    onDelete={() => handleTagDelete(tag)}
-                  ></Hashtag>
-                ))}
-              </S.HashtagContainer>
-
-              <Input
-                type="text"
-                errors={errors}
-                style={{ borderRadius: '4px', margin: '0' }}
-                onKeyUp={handleKeyUp}
-                {...register('hashtag')}
-              />
             </S.InputContainer>
 
             <S.ButtonContainer>
