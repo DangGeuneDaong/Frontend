39469950a1fa4385cb7937dff38a3a9dc059e12c
eeseohyun <jinlijinyoung@gmail.com>
2023-06-21 08:29:11 +0900

    Feat : 엑세스 토큰 상태관리 atom

--- src/states/authState.tsx
+++ src/states/authState.tsx
@@ -4,3 +4,11 @@ export const accessTokenState = atom<string | null>({
   key: 'accessTokenState', //state명
   default: null, //초기값
 });
+export const kakaoAccessTokenState = atom<string | null>({
+  key: 'kakaoAccessTokenState', //state명
+  default: null, //초기값
+});
+export const naverAccessTokenState = atom<string | null>({
+  key: 'naverAccessTokenState',
+  default: null,
+});
