6519e336c0f2f6046231175305a0a20eb648f42c
eeseohyun <jinlijinyoung@gmail.com>
2023-06-21 08:33:56 +0900

    Feat : 라우터 추가 (로그인, 소셜로그인 redirect)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
 
 import MainPage from './pages/MainPage/MainPage';
+import RedirectKakaoPage from './pages/RedirectPage/Kakao';
+import RedirectNaverPage from './pages/RedirectPage/Naver';
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<MainPage />} />
+      <Route path="/signin" element={<LoginPage />} />
+      <Route path="/redirect-kakao" element={<RedirectKakaoPage />} />
+      <Route path="/redirect-naver" element={<RedirectNaverPage />} />
     </Routes>
   );
 }
