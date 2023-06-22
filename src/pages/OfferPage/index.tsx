49b376845842e41b96685b9c13ba57701c2d99b6
geniee1220 <geniee1220@gmail.com>
2023-06-22 10:28:30 +0900

    Refactor : 변수 이름 변경 및 axiosInstance 적용, 채팅창 오픈된 상태에서 클릭 액션 변경

--- src/pages/OfferPage/index.tsx
+++ src/pages/OfferPage/index.tsx
@@ -1,7 +1,7 @@
 import * as S from './styles';
 
 import { useEffect, useState } from 'react';
-import axios from 'axios'
+import axios, { AxiosInstance } from 'axios';
 
 import ImageCarouselArea from '../../components/ImageCarouselArea';
 import PostArea from '../../components/PostArea';
@@ -12,6 +12,7 @@ import ChatRoomArea from '../../components/ChatRoomArea';
 import EditArea from '../../components/EditArea';
 import Pagination from '../../components/Pagination';
 import Button from '../../components/Button';
+import axiosInstance from '../../api';
 
 const config = [
   {
@@ -30,59 +31,74 @@ const config = [
 
 interface DataProps {
   id?: number;
-  nickname?: string,
-  distance?: string
+  nickname?: string;
+  distance?: string;
 }
 
 function OfferPage() {
-  const [showImages, setShowImages] = useState([])
-  const [showPosts, setShowPosts] = useState([])
-  const [showTakerlists, setShowTakerlists] = useState([])
-  const [limit, setLimit] = useState(5);
-  const [page, setPage] = useState(1);
-  const offset = (page - 1) * limit;
-  const [isChat, setIsChat] = useState<number>(-1) // 클릭할 때, 채팅창 보여주거나 가리는 state 기능
-  // const [isOpenChat, setIsOpenChat] = useState(false)
-  const [isSharing, setIsSharing] = useState<string>('') // 나눔완료 클릭 시, 나눔 중에서 완료로 변경
+  const [showImages, setShowImages] = useState([]);
+  const [showPosts, setShowPosts] = useState([]);
+  const [showTakerlists, setShowTakerlists] = useState([]);
 
+  const LIMIT = 5;
+  const [page, setPage] = useState(1);
+  const offset = (page - 1) * LIMIT;
+
+  const [selectedButtonId, setSelectedButtonId] = useState<number | null>(null); // 클릭할 때, 채팅창 보여주거나 가리는 state 기능
+  const [isOpenChat, setIsOpenChat] = useState(false);
+  const [isSharing, setIsSharing] = useState<string>(''); // 나눔완료 클릭 시, 나눔 중에서 완료로 변경
 
   useEffect(() => {
-    const SERVER_URL = 'http://localhost:5000'
+    const SERVER_URL = 'http://localhost:5000';
     const fetchData = async () => {
-      const { data } = await axios.get(`${SERVER_URL}/Good`)
-      setShowPosts(data)
+      const instance: AxiosInstance = axiosInstance();
+      const { data } = await instance.get(`${SERVER_URL}/Good`);
+      setShowPosts(data);
       // const result_images = await axios.get(`${SERVER_URL}/images`)
       // setShowImages(result_images.data)
-      const result_takerlists = await axios.get(`${SERVER_URL}/Sharing_Application`)
-      setShowTakerlists(result_takerlists.data)
-    }
-    fetchData()
-  }, [])
+      const result_takerlists = await instance.get(
+        `${SERVER_URL}/Sharing_Application`
+      );
+      setShowTakerlists(result_takerlists.data);
+    };
+    fetchData();
+  }, []);
 
-  const [selectedUserData, setSelectedUserData] = useState<DataProps>({ id: -1, nickname: '', distance: '' })
+  // 디버깅용
+  useEffect(() => {
+    console.log('selectedButtonId', selectedButtonId);
+  }, [selectedButtonId]);
+
+  const [selectedUserData, setSelectedUserData] = useState<DataProps>({
+    id: -1,
+    nickname: '',
+    distance: '',
+  });
 
   const showChange = (id: number, nickname?: string, distance?: string) => {
-    if (isChat === -1) {
-      setIsChat(id)
+    // 만약 버튼을 클릭했을 때, 현재 유저와 같은 유저라면 채팅창 닫기(초기화)
+    if (selectedButtonId === id) {
+      setSelectedButtonId(null);
+      setIsOpenChat(false);
     } else {
-      setIsChat(-1)
+      setSelectedButtonId(id);
+      setIsOpenChat(true);
     }
 
     // map으로 보낸 데이터 저장
     const data = {
       id: id,
       nickname: nickname,
-      distance: distance
-    }
+      distance: distance,
+    };
     // data 내 저장한 값을 외부에서 사용
-    setSelectedUserData(data)
-  }
+    setSelectedUserData(data);
+  };
 
   const changeSharing = (status: string) => {
-    if (status === "나눔 중") {
-
+    if (status === '나눔 중') {
     }
-  }
+  };
 
   return (
     <MainTemplate>
@@ -91,21 +107,33 @@ function OfferPage() {
           {/* {showImages.map(({ images }) => ())} */}
           <ImageCarouselArea config={config} />
 
-          {showPosts.map(({ id, main_category, sub_category, title, description, status, good_image_list, created_at, user_id }) => (
-            <>
-              <PostArea
-                key={id}
-                nickname={user_id["nickname"]}
-                location={user_id["location"]}
-                status={status}
-                title={title}
-                firstCategory={main_category}
-                secondCategory={sub_category}
-                createdTime={created_at}
-                productDetails={description}
-              />
-            </>
-          ))}
+          {showPosts.map(
+            ({
+              id,
+              main_category,
+              sub_category,
+              title,
+              description,
+              status,
+              good_image_list,
+              created_at,
+              user_id,
+            }) => (
+              <>
+                <PostArea
+                  key={id}
+                  nickname={user_id['nickname']}
+                  location={user_id['location']}
+                  status={status}
+                  title={title}
+                  firstCategory={main_category}
+                  secondCategory={sub_category}
+                  createdTime={created_at}
+                  productDetails={description}
+                />
+              </>
+            )
+          )}
           {/* <EditArea /> */}
           <S.EditContainer>
             <Button>삭제하기</Button>
@@ -113,32 +141,43 @@ function OfferPage() {
             <Button>나눔완료</Button>
           </S.EditContainer>
           <S.ListTitleContainer>Taker 목록</S.ListTitleContainer>
-          {showTakerlists.slice(offset, offset + limit).map(({ id, nickname, distance, content }) => (
-            <>
-              <TakerListArea
-                key={id}
-                nickname={nickname}
-                distance={distance}
-                content={content}
-                setProps={() => showChange(id, nickname, distance)}
-              >{isChat === id ? '숨기기' : '채팅하기'}</TakerListArea>
-            </>
-          ))}
+          {showTakerlists
+            .slice(offset, offset + LIMIT)
+            .map(({ id, nickname, distance, content }) => (
+              <>
+                <TakerListArea
+                  key={id}
+                  nickname={nickname}
+                  distance={distance}
+                  content={content}
+                  setProps={() => showChange(id, nickname, distance)}
+                >
+                  {selectedButtonId === id ? '숨기기' : '채팅하기'}
+                </TakerListArea>
+              </>
+            ))}
 
           <Pagination
             total={showTakerlists.length}
-            limit={limit}
+            limit={LIMIT}
             page={page}
             setPage={setPage}
           />
         </S.OfferContainer>
 
-
-        {/* {showTakerlists.map(({ id, nickname, distance }) => ())} */}
-        {isChat !== -1 && <ChatRoomArea nickname={selectedUserData.nickname} distance={selectedUserData.distance} />}
+        {isOpenChat && (
+          <ChatRoomArea
+            onClose={() => {
+              setIsOpenChat(false);
+              setSelectedButtonId(null);
+            }}
+            nickname={selectedUserData.nickname}
+            distance={selectedUserData.distance}
+          />
+        )}
       </S.Container>
     </MainTemplate>
   );
-};
+}
 
 export default OfferPage;
