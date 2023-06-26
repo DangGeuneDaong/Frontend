0a36aef6114f9b177d3b67fa03a216466d33e617
whale <whale2200d@gmail.com>
2023-06-26 14:50:21 +0900

    Refactor : Merge Conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
 
 import MainPage from './pages/MainPage/MainPage';
 import TakerPage from './pages/TakerPage';
 import OfferPage from './pages/OfferPage';
+import AddInfoPage from './pages/RedirectPage/AddInfoPage';
+import RedirectKakaoPage from './pages/RedirectPage/Kakao';
+import RedirectNaverPage from './pages/RedirectPage/Naver';
 
 export default function App() {
   return (
@@ -10,6 +14,10 @@ export default function App() {
       <Route path="/" element={<MainPage />} />
       <Route path="/taker" element={<TakerPage />} />
       <Route path="/offer" element={<OfferPage />} />
+      <Route path="/signin" element={<LoginPage />} />
+      <Route path="/redirect-kakao" element={<RedirectKakaoPage />} />
+      <Route path="/redirect-naver" element={<RedirectNaverPage />} />
+      <Route path="/addInfo" element={<AddInfoPage />} />
     </Routes>
   );
 }
