2df099f7679f06401daef11f9af68efadd62a3d8
geniee1220 <geniee1220@gmail.com>
2023-06-26 12:43:17 +0900

    Refactor : 더미 데이터 경로 변경 및 변수명 수정

--- src/apis/good/index.ts
+++ src/apis/good/index.ts
@@ -1,35 +1,38 @@
 import { AxiosInstance } from 'axios';
 import axiosInstance from '..';
 import { useRecoilState, useRecoilValue } from 'recoil';
-import { PostModel, postState, postStateLoading } from '../../states/goodstate';
+import { PostModel, postState, postStateLoading } from '../../states/goodState';
 import { useQuery } from 'react-query';
 import { useEffect } from 'react';
 
 // 이미지 업로드
-export const uploadImage = async (selecteeFiles: File[]) => {
+export const uploadImage = async (selectedFiles: File[]) => {
   const instance: AxiosInstance = axiosInstance();
   const formData = new FormData();
 
   // 업로드 한 이미지가 없다면 빈 배열을 return
-  if (selecteeFiles.length === 0) return [];
+  if (selectedFiles.length === 0) return [];
 
   try {
     console.log('파일 업로드 시작');
 
     //selectedItem의 각 아이템을 formData로 가공
-    for (let i = 0; i < selecteeFiles.length; i++) {
-      formData.append(`image${i}`, selecteeFiles[i]);
+    for (let i = 0; i < selectedFiles.length; i++) {
+      formData.append(`image${i}`, selectedFiles[i]);
     }
 
     // S3에서 각각의 이미지에 대해 String 값으로 return
     // const response = await instance.post('/api/img/upload', formData);
-    const response = await instance.post(
-      'http://localhost:5000/image',
-      formData
-    );
+    // const response = await instance.post(
+    //   'http://localhost:5000/image',
+    //   formData
+    // );
 
-    // return response;
-    return ['https://image.com/1', 'https://image.com/2'];
+    const response = selectedFiles.map((_, index) => {
+      return `https://dummyimage.com/420x320/ff7f7f/333333.png&text=Sample${index}`;
+    });
+
+    return response;
   } catch (error) {
     console.error(error);
     return [];
@@ -52,7 +55,6 @@ export const fetchPost = async (postId: string) => {
 
 // 개별글 조회 데이터 패치 커스텀 훅
 export const useFetchPost = (postId: string) => {
-  // TODO : API 연동할 때 any 타입을 수정
   const [post, setPost] = useRecoilState<any>(postState);
 
   const [isPostLoading, setIsPostLoading] = useRecoilState(postStateLoading);
@@ -97,7 +99,7 @@ export const addPost = async (data: any) => {
 export const editPost = async (data: any) => {
   try {
     const instance: AxiosInstance = axiosInstance();
-    const response = await instance.put('http://localhost:5000/good', data);
+    const response = await instance.put(`http://localhost:5000/good/3`, data);
 
     return response;
   } catch (error: any) {
