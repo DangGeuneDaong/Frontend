43217b37b9697876ed78538ff19371cf21916319
minkyuu <minkyu023@gmail.com>
2023-06-26 13:56:10 +0900

    Feat: Type수정 및 추가
    - 수정 (ItemType, CategoryType)
    - 추가 (StatusType)

--- src/components/KakaoMapContainer/itemType.ts
+++ src/components/KakaoMapContainer/itemType.ts
@@ -1,15 +1,30 @@
+type keyValueType = {
+  [key: string]: string;
+}
+
 export interface ItemType {
-  id: string;
-  location: string;
+  id: number;
+  title: string;
+  main_category: string;
   category: string;
   latitude: number;
   longitude: number;
-  title: string;
   address: string;
+  status: string;
+  good_image_list: string[];
 }
 
-export const CategoryType = {
-  food: '사료',
-  snack: '간식',
-  supply: '용품'
+export const StatusType: keyValueType = {
+  'sharing': '나눔중',
+  'shared': '나눔완료'
 };
+
+export const CategoryType: keyValueType = {
+  'food': '사료',
+  'snack': '간식',
+  'supply': '용품'
+};
+
+
+
+
