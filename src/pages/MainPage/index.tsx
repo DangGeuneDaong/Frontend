8e5a9efde36fd42ec169298eabbcae61a61094f8
minkyuu <minkyu023@gmail.com>
2023-06-26 16:48:47 +0900

    Chore: dependency 추가

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -6,12 +6,13 @@ import ItemFilterButton from '../../components/ItemFilterButton';
 import ItemCategory from '../../components/ItemCategory';
 import ItemList from '../../components/ItemList';
 import KakaoMapContainer from '../../components/KakaoMapContainer';
-import Pagination from '../../components/Pagination';
+import PaginationMK from '../../components/PaginationMK';
 
 import { ItemType } from '../../components/KakaoMapContainer/itemType';
 import uploadPostImg from '../../assets/imgs/edit.png';
 
 import * as S from './styles';
+import MainTemplate from '../../components/template/MainTemplate';
 
 const MainPage = () => {
   const [itemList, setItemList] = useState<ItemType[]>([]);
@@ -25,31 +26,33 @@ const MainPage = () => {
   const [currentPage, setCurrentPage] = useState<number>(1);
 
   return (
-    <S.Container>
-      <S.ItemInfo isOpenModal={isShowFilterModal} >
-        <ItemSearchBar onChangeKeyword={setKeyword} setPage={setCurrentPage}/>
-        <S.ItemFilter>
-          <ItemFilterButton isActive={isShowFilterModal} onClick={setIsShowFilterModal} />
-          <ItemCategory onSelectCategory={setCategory} setPage={setCurrentPage} />
-        </S.ItemFilter>
-        <ItemList items={itemList} />
-        <Pagination items={itemList} currentPage={currentPage} onMovePage={setCurrentPage}/>
-      </S.ItemInfo>
-      {isShowFilterModal && <ItemFilter onClose={setIsShowFilterModal} condition={filterCondition} onSelectFilter={setFilterCondition} setPage={setCurrentPage}/>}
-      <S.UploadPost to='/upload'>
-        <S.UploadPostImg src={uploadPostImg} alt='나눔 글 작성 버튼' />
-        나눔 글 작성
-      </S.UploadPost>
+    <MainTemplate>
+      <S.Container>
+        <S.ItemInfo isOpenModal={isShowFilterModal} >
+          <ItemSearchBar onChangeKeyword={setKeyword} setPage={setCurrentPage}/>
+          <S.ItemFilter>
+            <ItemFilterButton isActive={isShowFilterModal} onClick={setIsShowFilterModal} />
+            <ItemCategory onSelectCategory={setCategory} setPage={setCurrentPage} />
+          </S.ItemFilter>
+          <ItemList items={itemList} />
+          <PaginationMK items={itemList} currentPage={currentPage} onMovePage={setCurrentPage}/>
+        </S.ItemInfo>
+        {isShowFilterModal && <ItemFilter onClose={setIsShowFilterModal} condition={filterCondition} onSelectFilter={setFilterCondition} setPage={setCurrentPage}/>}
+        <S.UploadPost to='/upload'>
+          <S.UploadPostImg src={uploadPostImg} alt='나눔 글 작성 버튼' />
+          나눔 글 작성
+        </S.UploadPost>
 
-      <KakaoMapContainer
-        items={itemList}
-        category={category}
-        condition={filterCondition}
-        keyword={keyword}
-        onSearchItems={setItemList}
-        currentPage={currentPage}
-      />
-    </S.Container>
+        <KakaoMapContainer
+          items={itemList}
+          category={category}
+          condition={filterCondition}
+          keyword={keyword}
+          onSearchItems={setItemList}
+          currentPage={currentPage}
+        />
+      </S.Container>
+    </MainTemplate>
   );
 };
 
