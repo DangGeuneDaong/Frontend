d351559f6d11820885e1f6a037b17e07565970c3
eeseohyun <jinlijinyoung@gmail.com>
2023-06-28 20:15:24 +0900

    Refactor : 회원가입 라우터 변경

--- src/App.tsx
+++ src/App.tsx
@@ -12,15 +12,14 @@ import JoinPage from './pages/JoinPage';
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<MainPage />}/>
+      <Route path="/" element={<MainPage />} />
       <Route path="/taker" element={<TakerPage />} />
       <Route path="/offer" element={<OfferPage />} />
       <Route path="/signin" element={<LoginPage />} />
       <Route path="/redirect-kakao" element={<RedirectKakaoPage />} />
       <Route path="/redirect-naver" element={<RedirectNaverPage />} />
       <Route path="/addInfo" element={<AddInfoPage />} />
-
-      <Route path="/register" element={<JoinPage />} />
+      <Route path="/signup" element={<JoinPage />} />
     </Routes>
   );
 }
