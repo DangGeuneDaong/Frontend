088203219a96fe5525be40d2d2cbcb0021dda383
eeseohyun <jinlijinyoung@gmail.com>
2023-06-28 17:04:07 +0900

    Feat : mypage 라우터 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import MainPage from './pages/MainPage/MainPage';
+import MyPage from './pages/MyPage';
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<MainPage />} />
+      <Route path="/mypage" element={<MyPage />} />
     </Routes>
   );
 }
