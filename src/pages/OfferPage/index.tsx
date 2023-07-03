d22b766ec70f9da7b236bf5467ba0a789568a52d
whale <whale2200d@gmail.com>
2023-07-04 00:37:02 +0900

    Refactor : patch 대신 put

--- src/pages/OfferPage/index.tsx
+++ src/pages/OfferPage/index.tsx
@@ -120,7 +120,7 @@ function OfferPage() {
   };
 
   const onClickStatusHandler = async () => {
-    const { data } = await instance.patch(
+    const { data } = await instance.put(
       `${SERVER_URL}/good/offer/info?goodId=${param}`
     ); // 구조 분해 할당
     if (showPosts) {
