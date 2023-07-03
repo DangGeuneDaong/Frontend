dd2ef8d70014d6caf725519a1e8eae37aca175f0
geniee1220 <geniee1220@gmail.com>
2023-07-03 21:36:09 +0900

    Refactor : 개별글 조회 엔드포인트 수정

--- src/apis/good/index.ts
+++ src/apis/good/index.ts
@@ -39,7 +39,9 @@ export const fetchPost = async (postId: string) => {
   try {
     console.log('개별글 조회 시작');
 
-    const post = await instance.get(`http://13.209.220.63/good/${postId}`);
+    const post = await instance.get(
+      `http://13.209.220.63/good/offer/info?goodId/${postId}`
+    );
 
     return post.data;
   } catch (error: any) {
