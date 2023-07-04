48e9251e91736b0d7ec4f1aeb21f0221ff184f1f
whale <whale2200d@gmail.com>
2023-07-04 17:56:18 +0900

    Refactor : Token값 확인하기 위한 콘솔로그 추가

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -71,9 +71,12 @@ export function useAuth<T extends { [key: string]: any }>() {
     setError(null);
     try {
       const response = await loginRequest(data);
+      console.log(`accessToken1 : `, response.data.accessToken);
       if (response.data.accessToken) {
         localStorage.setItem('accessToken', response.data.accessToken);
+        console.log(`accessToken2 : `, response.data.accessToken);
         localStorage.setItem('userId', data.userId);
+        console.log(`userId : `, data.userId);
       }
 
       //유저 정보 가져와 상태로 관리
