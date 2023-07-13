464f8d3df99fff73f5a4bd6739234cec76900039
eeseohyun <jinlijinyoung@gmail.com>
2023-07-13 22:24:46 +0900

    Refactor : 코드삭제

--- src/states/userInfo.ts
+++ src/states/userInfo.ts
@@ -1,10 +1,5 @@
 import { atom } from 'recoil';
 
-export const userInfoState = atom<any>({
-  key: 'userInfoState',
-  default: {},
-});
-
 export const userState = atom<any>({
   key: 'userState',
   default: {},
